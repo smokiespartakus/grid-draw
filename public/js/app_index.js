9082bf4776d11bcb00e1d4e35d2f28833010fc72
Jonas Emil <jonasea@gmail.com>
2021-12-31 09:53:13 +0100

    index forms submit fixes

--- public/js/app_index.js
+++ public/js/app_index.js
@@ -2,11 +2,12 @@ new Vue({
 	el: '#app',
 	data: {
 		gameId: '',
-		width: 12,
+		width: 16,
 		height: 12,
 	},
 	methods: {
 		async create() {
+			console.log('create', this.height, this.width);
 			if (this.height <= 0 || this.width <= 0) return;
 			const response = await axios.post('/action/create', {w: this.width, h: this.height});
 			if(response.data && response.data.gameId) {
